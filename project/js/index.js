9ff75357cc3dc9d539a7e96435209aedd42f08f0
Marina Iv <marisha@marinas-mbp.myfiosgateway.com>
2021-08-26 19:16:26 -0400

    Fetched data from my Github

--- project/js/index.js
+++ project/js/index.js
@@ -14,23 +14,20 @@ for (let i = 0; i < skills.length; i ++) {
 
 //PROJECTS
 
+/*
 //for each AJAXrequest new XHR object should be created
 let  githubRequest = new XMLHttpRequest();
-
-
 //craete callback function - asynchronous part of AJAX - to trigger the call back we use a special browser event
 githubRequest.onreadystatechange= function () {
 
     if (githubRequest.readyState === 4) {
-   let repositories = JSON.parse(this.response);
-   console.log(repositories);
+    let repositories = JSON.parse(this.response);
+    console.log(repositories);
    
     let projectSection = document.getElementById("projects");
     let projectList = projectSection.querySelector("ul");
 
     for (let i  = 0; i < repositories.length; i++){
-
-
       let project = document.createElement("a");
       project.href = repositories[i].html_url; 
       project.target = "_blank"  
@@ -44,17 +41,52 @@ githubRequest.onreadystatechange= function () {
 
       projectList.appendChild(project);
       projectList.appendChild(projectlan);
-
     }
   } 
 }
-
 //sending the request
 githubRequest.open("GET", "https://api.github.com/users/krapivaa/repos");
 githubRequest.send();
+*/
 
 
+//FETCH API
+fetch ("https://api.github.com/users/krapivaa/repos", {
+    method: "GET"
+    })
+    .then(response => response.json())
+    .then(data => githubRequest(data))
+    .catch((error) => {
+        console.error("Error:", error);
+    })
 
+    function githubRequest(data) {
+
+        let projectSection = document.getElementById("projects");
+        let projectList = projectSection.querySelector("ul");
+    
+        for (let i  = 0; i < data.length; i++){
+
+         let projectItem  =  document.createElement("li");
+
+          let project = document.createElement("a");
+          project.href = data[i].html_url; 
+          project.target = "_blank"  
+    
+          project.innerText = data[i].name.toUpperCase(); 
+          project.className  = "projectUl";
+    
+          let projectlan = document.createElement("li");
+          projectlan.innerText = data[i].language;   
+          projectlan.style.fontStyle = "italic"; 
+
+          projectItem.appendChild(project);
+          projectItem.appendChild(projectlan);
+          projectList.appendChild(projectItem);
+        
+        }
+      } 
+  
 
 //MESSAGES
 let messageForm = document.getElementsByName("leave_message")[0];
